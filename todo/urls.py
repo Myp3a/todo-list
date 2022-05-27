ee8df237294057ea3120db0b6747aa4d6ee0fd98
Myp3a <murza8965@gmail.com>
2022-05-27 20:23:09 +0300

    Ending slashes fix

--- todo/urls.py
+++ todo/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('auth/', views.auth, name="auth"),
-    path('reg/', views.reg, name="reg"),
-    path('task/<int:id>/', views.detail, name='taskdata'),
-    path('add/', views.add, name="add"),
+    path('auth', views.auth, name="auth"),
+    path('reg', views.reg, name="reg"),
+    path('task/<int:id>', views.detail, name='taskdata'),
+    path('add', views.add, name="add"),
     path('logout', views.logout, name="logout"),
     path('delete', views.delete, name="delete"),
     path('toggle', views.toggle, name="toggle")
